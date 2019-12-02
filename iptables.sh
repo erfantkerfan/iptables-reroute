1a06f6fde82badc31d7c23acb17fa52c6aad37eb
Erfan Gholizade <erfantkerfan@yahoo.com>
2019-12-02 18:02:55 +0330

    first commit

---
+++ iptables.sh
@@ -0,0 +1,3 @@
+sudo iptables -I INPUT -p tcp -m tcp --dport 8080 -j ACCEPT
+sudo iptables -t nat -A PREROUTING -p tcp --dport 8080 -j DNAT --to-destination 1.1.1.1:8080
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
